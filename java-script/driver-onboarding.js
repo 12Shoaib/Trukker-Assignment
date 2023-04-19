a44f49d9db9b46cd98297c99dad4d24a7eef9cef
shoaib <shoaibmohd916@gmail.com>
2023-04-20 04:08:32 +0530

    Form Validations completed

--- java-script/driver-onboarding.js
+++ java-script/driver-onboarding.js
@@ -1,7 +1,12 @@
 let driversDetails = []  //Seeding data
+const mandatoryField = ["f-name","m-name","l-name","m-number","nationality",
+"country-o-b","city-o-b","card-name","street","country","city-town","z-code"]
+
+//"select-country" , "gender"
 
 function submitDetails(event){
     event.preventDefault()
+    validateAge()
     let firstName = document.getElementById("f-name")
     let middleName = document.getElementById("m-name")
     let lastName = document.getElementById("l-name")
@@ -43,8 +48,50 @@ function submitDetails(event){
        customerId : "ARB"+ Math.floor(Math.random()*10000000),
        cardNo : "**** **** **** "+Math.floor(Math.random()*10000)
     }
-    console.log(newDriverData)
-    driversDetails = [ newDriverData  , ...driversDetails ]
-    localStorage.setItem("DriverDetails", JSON.stringify(driversDetails))
+
+    if(validateInputFields()){
+        console.log(newDriverData)
+        driversDetails = [ newDriverData  , ...driversDetails ]
+        localStorage.setItem("DriverDetails", JSON.stringify(driversDetails))
+        alert("Congratulations you have been added")
+    }
+    else{
+        for(let i=0 ; i<mandatoryField.length; i++){
+            const currentInputField = document.getElementById(mandatoryField[i])
+            const inputCapture = currentInputField.value    
+    
+            if(inputCapture === ""){
+                currentInputField.classList.add('alert')
+                emptyFields = true
+            }else{
+                currentInputField.classList.remove('alert')
+            }   
+        }
+    }
+}   
+
+const validateInputFields = () => {
+     
+     for(let i=0 ; i<mandatoryField.length; i++){
+        const currentInputField = document.getElementById(mandatoryField[i])
+        const inputCapture = currentInputField.value    
+        console.log(inputCapture)
+        if(inputCapture === ""){
+            return false
+        }else{
+            return true
+        }
+     }
+
 }
 
+const validateAge = () => {
+    let dateOfBirth = new Date(document.getElementById("dob").value)
+    let presentDate = new Date
+
+    let presentAge =   presentDate.getFullYear() - dateOfBirth.getFullYear()
+
+    if(presentAge <= 18 ){
+        alert("Your age is less than 18")
+    }
+}

231c43b468a3af57563572fd20adf5be48ee63f6
shoaib <shoaibmohd916@gmail.com>
2023-04-20 15:43:48 +0530

    Dropdwon with api integration

--- java-script/driver-onboarding.js
+++ java-script/driver-onboarding.js
@@ -1,4 +1,5 @@
 let driversDetails = []  //Seeding data
+
 const mandatoryField = ["f-name","m-name","l-name","m-number","nationality",
 "country-o-b","city-o-b","card-name","street","country","city-town","z-code"]
 
@@ -25,12 +26,13 @@ function submitDetails(event){
     let city = document.getElementById("city-town")
     let zipCode = document.getElementById("z-code")
 
+    
     const newDriverData = {
        firstName: firstName.value,
        middleName: middleName.value ,
        lastName : lastName.value,
        gender: gender.value,
-       fNameArabic: fNameArabic.value,
+       fNameArabic: "قبول کرنے وال",
        mNameArabic: mNameArabic.value,
        lNameArabic: lNameArabic.value,
        dateOfBirth: dateOfBirth.value,
@@ -46,12 +48,14 @@ function submitDetails(event){
        moxeyId : "MXUAE-DR"+Math.floor(Math.random()*10000000),
        transporter : 'Trukker',
        customerId : "ARB"+ Math.floor(Math.random()*10000000),
-       cardNo : "**** **** **** "+Math.floor(Math.random()*10000)
+       cardNo : "**** **** **** "+Math.floor(Math.random()*10000),
+       status : "IN-PROCESS",
+       eye: "eye"
     }
 
     if(validateInputFields()){
-        console.log(newDriverData)
-        driversDetails = [ newDriverData  , ...driversDetails ]
+        const data = JSON.parse(localStorage.getItem("DriverDetails"))
+        driversDetails = [ newDriverData  , ...driversDetails , ] 
         localStorage.setItem("DriverDetails", JSON.stringify(driversDetails))
         alert("Congratulations you have been added")
     }
@@ -69,13 +73,11 @@ function submitDetails(event){
         }
     }
 }   
-
 const validateInputFields = () => {
      
      for(let i=0 ; i<mandatoryField.length; i++){
         const currentInputField = document.getElementById(mandatoryField[i])
         const inputCapture = currentInputField.value    
-        console.log(inputCapture)
         if(inputCapture === ""){
             return false
         }else{
@@ -94,4 +96,65 @@ const validateAge = () => {
     if(presentAge <= 18 ){
         alert("Your age is less than 18")
     }
+
 }
+let globalCountry = document.getElementById("myDropdown")
+let countryOfBirth = document.getElementById("country-o-b")
+let cityOfBirth = document.getElementById("city-o-b")
+let presentCountry = document.getElementById("Present-country")
+let presentCountryCity = document.getElementById("town")
+let api = 'https://api.countrystatecity.in/v1/countries'
+let key = 'RGhzemVIZFQ2VWZWRGdUb2UwMUlBeW9MUEZyejd0dUwzaUhsYWExbQ=='
+
+
+fetch(api, {headers: {'X-CSCAPI-KEY': key}})
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(country => {
+        let getOption = document.createElement('option')
+        getOption.value = country.iso2
+        getOption.text = country.name
+        globalCountry.appendChild(getOption) 
+
+        let getOption1 = document.createElement('option')
+        getOption1.value = country.iso2
+        getOption1.text = country.name
+        countryOfBirth.appendChild(getOption1)
+
+        let getOption2 = document.createElement('option')
+        getOption2.value = country.iso2
+        getOption2.text  = country.name
+        presentCountry.appendChild(getOption2)
+    })
+    })
+  .catch(error => {
+    console.log(error)
+  })
+
+const integratingCities = ( captured  , cityOfBirth  ) => {
+    const api = `https://api.countrystatecity.in/v1/countries/${captured}/cities`
+    console.log(captured)
+    fetch(api , {headers: {'X-CSCAPI-KEY': key}})
+    .then(response => response.json()
+    )
+    .then(data => { 
+        data.forEach(cities => {
+            const option  = document.createElement('option')
+            option.value = cities.id
+            option.text = cities.name
+            cityOfBirth.appendChild(option)
+        })
+    })
+    
+}
+
+countryOfBirth.addEventListener('change' , () => {
+    const captured = countryOfBirth.value
+    integratingCities(captured , cityOfBirth )
+})
+
+
+presentCountry.addEventListener('change' , () => {
+    const captured = presentCountry.value
+    integratingCities(captured , presentCountryCity )
+})
--- java-script/index.js
+++ java-script/index.js
@@ -1,3 +1,34 @@
 const navigate = () => {
     location.href = "driver-onboarding.html"
 }
+
+const getDataFromLocal = () => {
+    const data = JSON.parse(localStorage.getItem("DriverDetails"))
+    const appendingDiv = document.getElementById('myDiv');
+    let emitingData  = ""
+    for(let i=0; i<data.length; i++){
+        emitingData += `<table class="table">
+        <tr>
+            <td> ${data[i].status}</td>
+            <td> ${data[i].moxeyId}</td>
+            <td>${data[i].transporter}</td>
+            <td> ${data[i].firstName}</td>
+            <td> ${data[i].fNameArabic}</td>
+            <td> ${data[i].mobileNumber}</td>
+            <td> ${data[i].customerId}</td>
+            <td> ${data[i].cardNo}</td>
+            <td> ${data[i].eye}</td>
+        </tr>
+        </table>`
+    }
+    appendingDiv.innerHTML = emitingData
+    inProcess()
+}
+setTimeout(getDataFromLocal , 1000)
+
+const inProcess = () => {
+    const data = JSON.parse(localStorage.getItem("DriverDetails"))
+    const tag = document.getElementById("c-i-p")
+    tag.innerHTML = data.length
+}
+
